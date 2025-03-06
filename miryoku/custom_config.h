e17caec12d708248e46581ef8c0fe13c641014d7
Sandy Jackson <sandyirjackson@gmail.com>
2025-03-06 18:09:53 +0000

    Added Miryoku options to custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,11 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
 
+
+#define MIRYOKU_CLIPBOARD_MAC
 #define HRM_TAPPING_TERM 400
 #define HRM_QUICK_TAP 175
 #define HRM_PRIOR_IDLE 280
