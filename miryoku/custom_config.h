08a7a6b87c3354af58ee6b8d56b4915a817c7fd2
Sandy Jackson <sandyirjackson@gmail.com>
2025-03-07 09:58:06 +0000

    swapped base button and mouse layer taps

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -14,8 +14,8 @@
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 HRM_L(LSHFT, A),     HRM_L(LALT, S),     HRM_L(LCTRL, D),    HRM_L(LGUI, F),    &kp G,             &kp H,             HRM_R(LGUI, J),    HRM_R(LCTRL, K),    HRM_R(LALT, L),     HRM_R(LSHFT, SQT),   \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_LT(U_MOUSE, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_BUTTON, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_EXTRA \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
