ee9beff1b9943862ab1ffbb19f18b047fde3ce62
Sandy Jackson <sandyirjackson@gmail.com>
2025-03-10 10:04:07 +0000

    Added FUN and MEDIA Custom mappings

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -24,10 +24,10 @@ MIRYOKU_X(BUTTON, "Button") \
 MIRYOKU_X(NAV,    "Nav") \
 MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym")
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun")
 //MIRYOKU_X(MOUSE,  "Mouse")
 //MIRYOKU_X(TAP,    "Tap")
-//MIRYOKU_X(FUN,    "Fun")
 
 #define U_BASE   0
 #define U_EXTRA  1
@@ -36,9 +36,7 @@ MIRYOKU_X(SYM,    "Sym")
 #define U_MEDIA  4
 #define U_NUM    5
 #define U_SYM    6
-#define U_TAP    U_BASE
-#define U_MOUSE  U_BASE
-#define U_FUN    U_BASE
+#define U_FUN    7
 
 // Custom Base Layer Mapping for Totem
 #define MIRYOKU_LAYOUTMAPPING_BASE( \
@@ -57,7 +55,7 @@ MIRYOKU_X(SYM,    "Sym")
 #define MIRYOKU_LAYER_BASE \
     &kp Q,            &kp W,             &kp E,             &kp R,             &kp T,                 &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
      HRM_L(LSHFT, A),   HRM_L(LALT, S),   HRM_L(LCTRL, D),  HRM_L(LGUI, F),       &kp G,                &kp H,             HRM_R(LGUI, J),    HRM_R(LCTRL, K),   HRM_R(LALT, L),  HRM_R(LSHFT, SQT), \
-    U_LT(U_BUTTON, Z),   U_MT(RALT, X),    &kp C,             &kp V,             &kp B,                &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+    U_LT(U_BUTTON, Z),   U_MT(RALT, X),    &kp C,             &kp V,             &kp B,                &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_FUN, SLASH),\
     U_NP,             U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC),    &m_shft,              U_LT(U_SYM, RET),U_LT(U_NUM,SPACE),U_MT(LS(LC(LALT)),TAB),   U_NP,             U_NP
 //╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯   ╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯
 
@@ -66,7 +64,7 @@ MIRYOKU_X(SYM,    "Sym")
 #define MIRYOKU_LAYER_EXTRA \
     &kp B,             &kp L,             &kp D,             &kp W,             &kp Z,                 &kp J,             &kp F,             &kp O,             &kp U,             &kp SQT,            \
     HRM_L(LSHFT, N),   HRM_L(LALT, R),    HRM_L(LCTRL, T),   HRM_L(LGUI, S),    &kp G,                 &kp Y,             HRM_R(LGUI, H),    HRM_R(LCTRL, A),   HRM_R(LALT, E),    HRM_R(LSHFT, I),   \
-    U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,                 &kp K,             &kp P,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+    U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,                 &kp K,             &kp P,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_FUN, SLASH),\
     U_NP,             U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC),    &m_shft,              U_LT(U_SYM, RET),U_LT(U_NUM,SPACE),U_MT(LS(LC(LALT)),TAB),   U_NP,             U_NP
 //╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯   ╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯
 
@@ -99,9 +97,9 @@ MIRYOKU_X(SYM,    "Sym")
 
 //╭──────────────────┬──────────────────┬──────────────────┬──────────────────┬──────────────────╮   ╭──────────────────┬──────────────────┬──────────────────┬──────────────────┬──────────────────╮
 #define MIRYOKU_LAYER_SYM \
-    &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,             U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     U_NA,  	         U_BOOT,            \
-    &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,             U_NA,              &kp LGUI,          &kp LCTRL,         &kp LALT,          &kp LSHFT,         \
-    &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,             U_NA,              &u_to_U_SYM,       U_NA,               &kp RALT,          U_NA,              \
+    &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,             U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     U_NA,  	         U_BOOT,      \
+    &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,             U_NA,              &kp LGUI,          &kp LCTRL,         &kp LALT,          &kp LSHFT,  \
+    &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,             U_NA,              &u_to_U_SYM,       U_NA,               &kp RALT,          U_NA,      \
     U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 //╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯   ╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯
 
@@ -111,4 +109,20 @@ MIRYOKU_X(SYM,    "Sym")
     &kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,            U_NA,              &kp LGUI,          &kp LCTRL,         &kp LALT,          &kp LSHFT,         \
     &kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,             U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
     U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+//╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯   ╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯
+
+//╭──────────────────┬──────────────────┬──────────────────┬──────────────────┬──────────────────╮   ╭──────────────────┬──────────────────┬──────────────────┬──────────────────┬──────────────────╮
+#define MIRYOKU_LAYER_MEDIA \
+    U_BOOT,            U_NA,              &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,                    U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         U_RGB_TOG,  \
+    &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,                    &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        U_EP_TOG,   \
+    U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,                    &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       &u_out_tog, \
+    U_NP,              U_NP,              U_NA,              U_NA,              U_NA,                    &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
+//╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯   ╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯
+
+//╭──────────────────┬──────────────────┬──────────────────┬──────────────────┬──────────────────╮   ╭──────────────────┬──────────────────┬──────────────────┬──────────────────┬──────────────────╮
+#define MIRYOKU_LAYER_FUN \
+    &kp F12,           &kp F7,            &kp F8,            &kp F9,            &kp PSCRN,               U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     U_NA,       U_BOOT,            \
+    &kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,                U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,   \
+    &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,         U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,       \
+    U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp TAB,                 U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 //╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯   ╰──────────────────┴──────────────────┴──────────────────┴──────────────────┴──────────────────╯
